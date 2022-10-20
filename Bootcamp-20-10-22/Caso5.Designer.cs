5823a03075ad18e80cb461c8b36661149d181fff
gaston migone <gsmigone@gmail.com>
2022-10-20 16:47:14 -0300

    update

---
+++ Bootcamp-20-10-22/Caso5.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Bootcamp_20_10_22
+{
+    partial class Caso5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVal = new System.Windows.Forms.Button();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtSex = new System.Windows.Forms.TextBox();
+            this.txtRol = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVal
+            // 
+            this.btnVal.Location = new System.Drawing.Point(180, 213);
+            this.btnVal.Name = "btnVal";
+            this.btnVal.Size = new System.Drawing.Size(75, 23);
+            this.btnVal.TabIndex = 0;
+            this.btnVal.Text = "Validar";
+            this.btnVal.UseVisualStyleBackColor = true;
+            this.btnVal.Click += new System.EventHandler(this.btnVal_Click);
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(167, 55);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNom.TabIndex = 1;
+            // 
+            // txtSex
+            // 
+            this.txtSex.Location = new System.Drawing.Point(167, 100);
+            this.txtSex.Name = "txtSex";
+            this.txtSex.Size = new System.Drawing.Size(100, 20);
+            this.txtSex.TabIndex = 2;
+            // 
+            // txtRol
+            // 
+            this.txtRol.Location = new System.Drawing.Point(167, 148);
+            this.txtRol.Name = "txtRol";
+            this.txtRol.Size = new System.Drawing.Size(100, 20);
+            this.txtRol.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sexo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(99, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rol:";
+            // 
+            // Caso5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 292);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRol);
+            this.Controls.Add(this.txtSex);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.btnVal);
+            this.Name = "Caso5";
+            this.Text = "Caso5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVal;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtSex;
+        private System.Windows.Forms.TextBox txtRol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Bootcamp-20-10-22/Caso5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bootcamp_20_10_22
+{
+    public partial class Caso5 : Form
+    {
+        public Caso5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVal_Click(object sender, EventArgs e)
+        {
+            string rol = txtRol.Text;
+            ValidarRol(rol);
+        }
+
+        private void ValidarRol(string rol)
+        {
+            switch (rol)
+            {
+                case "Administrador":
+                    Imprimir("El rol es Administrador");
+                    break;
+                case "Estandar":
+                    Imprimir("El rol es Estandar");
+                    break;
+                case "Invitado":
+                    Imprimir("El rol es Invitado");
+                    break;
+
+            }
+        }
+
+        public void Imprimir(string texto)
+        {
+            MessageBox.Show(texto);
+        }
+    }
+}
